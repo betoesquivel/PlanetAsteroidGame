85b49c716434e6387faaecf750942ac6eee81e5e
Beto Esquivel <beto.esquivel.p@gmail.com>
2014-01-27 21:46:54 -0600

    added animation explosion

--- src/huyendodelasteroide/Inteligencia.java
+++ src/huyendodelasteroide/Inteligencia.java
@@ -27,7 +27,6 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
 
     private static final long serialVersionUID = 1L;
     // Se declaran las variables.
-    private int direccion;    // Direccion del elefante
     private int incX;    // Incremento en x
     private int incY;    // Incremento en y
     private int vidas;    // vidas del elefante.
@@ -37,12 +36,18 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
     private Image gameover;    //Imagen a desplegar al acabar el juego.	 
     private Image live_image;        //Imagen a desplegar que representa una vida.
     private ImageIcon lives;
+    private Image explosion;
     private Graphics dbg;	// Objeto grafico
     private AudioClip bomb;    //Objeto AudioClip 
     private Planeta jupiter;    // Objeto de la clase Planeta
     private Asteroide asteroid;    //Objeto de la clase Asteroide
     private int speed; //contains the speed of the asteroid
     private boolean object_clicked;
+    private boolean is_exploding;
+    private int explosion_cycles;
+    private int explosion_cycles_counter;
+    private int explosion_Y;
+    private int explosion_X;
 
     /**
      * Metodo <I>init</I> sobrescrito de la clase <code>Applet</code>.<P>
@@ -75,6 +80,11 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
         URL livesURL = this.getClass().getResource("/images/whiterectangle.gif");
         live_image = Toolkit.getDefaultToolkit().getImage(livesURL);
         lives = new ImageIcon(live_image);
+
+        URL expURL = this.getClass().getResource("/images/explosion_bien.gif");
+        explosion = Toolkit.getDefaultToolkit().getImage(expURL);
+        explosion_cycles = 10;
+        explosion_cycles_counter = -1;
         object_clicked = false;
     }
 
@@ -122,22 +132,23 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
      */
     public void actualiza() {
         //Dependiendo de la direccion del elefante es hacia donde se mueve.
+        if (!is_exploding) {
+            //Acutalizo la posicion del asteroid
+            if (jupiter.getPosX() > asteroid.getPosX()) {
+                incX = speed;
+                asteroid.setPosX(asteroid.getPosX() + incX);
+            } else {
+                incX = -1 * speed;
+                asteroid.setPosX(asteroid.getPosX() + incX);
+            }
 
-        //Acutalizo la posicion del asteroid
-        if (jupiter.getPosX() > asteroid.getPosX()) {
-            incX = speed;
-            asteroid.setPosX(asteroid.getPosX() + incX);
-        } else {
-            incX = -1*speed;
-            asteroid.setPosX(asteroid.getPosX() + incX);
-        }
-
-        if (jupiter.getPosY() > asteroid.getPosY()) {
-            incY = speed;
-            asteroid.setPosY(asteroid.getPosY() + incY);
-        } else {
-            incY = -1*speed;
-            asteroid.setPosY(asteroid.getPosY() + incY);
+            if (jupiter.getPosY() > asteroid.getPosY()) {
+                incY = speed;
+                asteroid.setPosY(asteroid.getPosY() + incY);
+            } else {
+                incY = -1 * speed;
+                asteroid.setPosY(asteroid.getPosY() + incY);
+            }
         }
 
     }
@@ -147,23 +158,34 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
      * con las orillas del <code>Applet</code>.
      */
     public void checaColision() {
+        if (is_exploding && explosion_cycles_counter > 0) {
+            explosion_cycles_counter--;
+        } else {
+            if (jupiter.intersecta(asteroid) && !is_exploding) {
+                bomb.play();    //sonido al colisionar
+                explosion_cycles_counter = explosion_cycles;
+                is_exploding = true;
+                explosion_X = jupiter.getPosX();
+                explosion_Y = jupiter.getPosY();
 
-        if (jupiter.intersecta(asteroid)) {
-            bomb.play();    //sonido al colisionar
-            //El elefante se mueve al azar en la mitad izquierda del applet.
-            jupiter.setPosX((int) (Math.random() * (getWidth() / 2 - jupiter.getAncho())));
-            jupiter.setPosY((int) (Math.random() * (getHeight() / 2 - jupiter.getAlto())));
-            //El asteroid se mueve al azar en la mitad derecha del appler.
-            asteroid.setPosX((int) (Math.random() * getWidth() / 2) + getWidth() / 2 - asteroid.getAncho());
-            asteroid.setPosY((int) (Math.random() * getHeight() / 2) + getHeight() / 2 - asteroid.getAlto());
+                //El elefante se mueve al azar en la mitad izquierda del applet.
+                jupiter.setPosX((int) (Math.random() * (getWidth() / 2 - jupiter.getAncho())));
+                jupiter.setPosY((int) (Math.random() * (getHeight() / 2 - jupiter.getAlto())));
+                //El asteroid se mueve al azar en la mitad derecha del appler.
+                asteroid.setPosX((int) (Math.random() * getWidth() / 2) + getWidth() / 2 - asteroid.getAncho());
+                asteroid.setPosY((int) (Math.random() * getHeight() / 2) + getHeight() / 2 - asteroid.getAlto());
 
-            object_clicked = false;
+                object_clicked = false;
 
-            if (vidas > 0) {
-                vidas--;
-                speed++;
+                if (vidas > 0) {
+                    vidas--;
+                    speed++;
+                }
+            } else {
+                is_exploding = false;
             }
         }
+
     }
 
     /**
@@ -203,10 +225,6 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
     public void paint(Graphics g) {
         if (vidas > 0) {
             if (jupiter != null && asteroid != null) {
-                //Dibuja la imagen en la posicion actualizada
-                g.drawImage(jupiter.getImagenI(), jupiter.getPosX(), jupiter.getPosY(), this);
-                g.drawImage(asteroid.getImagenI(), asteroid.getPosX(), asteroid.getPosY(), this);
-
                 int text_length = 70;
                 g.setColor(Color.WHITE);
                 Font newf = g.getFont().deriveFont(Font.BOLD);
@@ -215,6 +233,14 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
                 for (int i = 0; i < vidas; i++) {
                     g.drawImage(lives.getImage(), text_length + i * lives.getIconWidth(), 0, this);
                 }
+                if (is_exploding) {
+                    g.drawImage(explosion, explosion_X, explosion_Y, this);
+                } else {
+                    //Dibuja la imagen en la posicion actualizada
+                    g.drawImage(jupiter.getImagenI(), jupiter.getPosX(), jupiter.getPosY(), this);
+                    g.drawImage(asteroid.getImagenI(), asteroid.getPosX(), asteroid.getPosY(), this);
+                }
+
             } else {
                 //Da un mensaje mientras se carga el dibujo	
                 g.drawString("No se cargo la imagen..", 20, 20);
@@ -231,8 +257,9 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
 
     /**
      * <I>mousePressed</I>
-     * Changes the state of the object to clicked when it is clicked
-     * to begin dragging.
+     * Changes the state of the object to clicked when it is clicked to begin
+     * dragging.
+     *
      * @param me contains the mouse event captured.
      */
     @Override
@@ -240,13 +267,14 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
         jupiter.setMx(me.getX());
         jupiter.setMy(me.getY());
         object_clicked = jupiter.planet_is_clicked();
-        
+
         me.consume();
     }
 
     /**
      * <I>mouseRealeased</I>
-     * Changes the state of the object to not clicked to stop dragging. 
+     * Changes the state of the object to not clicked to stop dragging.
+     *
      * @param me contains the mouse event captured
      */
     @Override
@@ -266,8 +294,9 @@ public class Inteligencia extends Applet implements Runnable, MouseListener, Mou
     }
 
     /**
-     * Activated when the mouse is dragged
-     * I check if it is in a clicked state so that it can be dragged.
+     * Activated when the mouse is dragged I check if it is in a clicked state
+     * so that it can be dragged.
+     *
      * @param me contains the mouse event
      */
     @Override
